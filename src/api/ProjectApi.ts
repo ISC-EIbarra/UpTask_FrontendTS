88d91b7a12c9326122f4f363e3b76c70fc0b8aed
ISC-EIbarra <EdsonP4100@gmail.com>
2024-06-26 15:19:05 -0700

    Add
    Create and edit functions

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,5 +1,9 @@
 import api from '@/lib/axios';
-import { ProjectFormData, dashboardProjectSchema } from '@/types/index';
+import {
+  Project,
+  ProjectFormData,
+  dashboardProjectSchema,
+} from '@/types/index';
 import { isAxiosError } from 'axios';
 
 export async function createProject(formData: ProjectFormData) {
@@ -20,6 +24,33 @@ export async function getProjects() {
     if (response.success) {
       return response.data;
     }
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjectById(id: Project['_id']) {
+  try {
+    const { data } = await api(`/projects/${id}`);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+type ProjectApiType = {
+  formData: ProjectFormData;
+  projectId: Project['_id'];
+};
+
+export async function updateProject({ formData, projectId }: ProjectApiType) {
+  try {
+    const { data } = await api.put<string>(`/projects/${projectId}`, formData);
+    return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
