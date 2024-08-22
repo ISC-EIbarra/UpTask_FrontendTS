02a880d0aa4c269806428be8dedef87e01fcfb0e
ISC-EIbarra <EdsonP4100@gmail.com>
2024-08-22 15:16:45 -0700

    Update AuthApi

---
+++ src/api/AuthApi.ts
@@ -0,0 +1,42 @@
+import api from '@/lib/axios';
+import {
+  ConfirmToken,
+  RequestConfirmationCodeForm,
+  UserRegistrationForm,
+} from '../types';
+import { isAxiosError } from 'axios';
+
+export async function createAccount(formData: UserRegistrationForm) {
+  try {
+    const { data } = await api.post<string>('/auth/create-account', formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function confirmAccount(formData: ConfirmToken) {
+  try {
+    const { data } = await api.post<string>('/auth/confirm-account', formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function requestConfirmationCode(
+  formData: RequestConfirmationCodeForm
+) {
+  try {
+    const { data } = await api.post<string>('/auth/request-code', formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
